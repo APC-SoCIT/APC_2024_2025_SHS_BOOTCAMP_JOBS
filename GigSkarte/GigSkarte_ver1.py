7625b835d6cfdedf8dc770402cc9b3f72e21ce06
Sofhian Mhyn Robles <srobles@student.apc.edu.ph>
2025-06-08 20:09:03 +0800

    test

--- GigSkarte/GigSkarte_ver1.py
+++ GigSkarte/GigSkarte_ver1.py
@@ -18,11 +18,11 @@ from kivy.properties import StringProperty
 from kivymd.font_definitions import theme_font_styles
 from kivy.core.window import Window
 
-# Database 
+# --- Database ---
 
 
 
-# 
+# ------------------
 
 Window.size = (360, 640)
 
@@ -598,6 +598,8 @@ class CombinedApp(MDApp):
             self.session = {}
 
     def build(self):
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "Blue"
         return Builder.load_file("GigSkarte_ver1.kv")
 
 
